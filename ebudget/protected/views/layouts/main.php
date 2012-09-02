51924a229da16ac9906f22974e2950e4a9b431d8
Abuthakir <abuthakir4u@gmail.com>
2012-09-02 21:41:10 +0530

    Added necessary header links
    Transactions and Transaction_categories links added in header menu

--- ebudget/protected/views/layouts/main.php
+++ ebudget/protected/views/layouts/main.php
@@ -32,6 +32,8 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Transactions', 'url'=>array('/transactions')),
+				array('label'=>'Transaction Categories', 'url'=>array('/transactionCategories')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
