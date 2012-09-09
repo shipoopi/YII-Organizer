9086aed98354d51b55fc19aba4eeec3e8ca6b743
Abuthakir <abuthakir4u@gmail.com>
2012-09-09 16:17:04 +0530

    Added generated code for settings module
    - Generated code with gii tool for setting module
    - settings table was auto generated when calling CmsSettings component
    set method

---
+++ ebudget/protected/views/settings/_form.php
@@ -0,0 +1,42 @@
+<?php
+/* @var $this SettingsController */
+/* @var $model Settings */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'settings-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'category'); ?>
+		<?php echo $form->textField($model,'category',array('size'=>60,'maxlength'=>64)); ?>
+		<?php echo $form->error($model,'category'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'key'); ?>
+		<?php echo $form->textField($model,'key',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'key'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'value'); ?>
+		<?php echo $form->textArea($model,'value',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'value'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
